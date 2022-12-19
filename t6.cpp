1688bee8e892033340d52f292afaf56e3b1bb93b
unknown <tayyabashraf629@gmail.com>
2022-12-19 07:34:40 -0800

    message

---
+++ t6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+system("Color 46");
+cout<<"*********************************************"<<endl;
+cout<<"*                                           *"<<endl;
+cout<<"*            University Addmission          *"<<endl;
+cout<<"*                                           *"<<endl;
+cout<<"*              MAnagement system            *"<<endl;
+cout<<"*                                           *"<<endl;
+cout<<"*                                           *"<<endl;
+cout<<"*                                           *"<<endl;
+cout<<"*********************************************"<<endl;
+}
---
+++ t8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+system("Color 37"); 
+cout<<"        -----------"<<endl;
+cout<<"      ---------------"<<endl;
+cout<<"     -------------------"<<endl;
+cout<<"    ----------------"<<endl;
+cout<<"   ------------"<<endl;
+cout<<"   -------------"<<endl;
+cout<<"    ----------------"<<endl;
+cout<<"     -------------------"<<endl;
+cout<<"      ---------------"<<endl;
+cout<<"        -----------"<<endl;
+}
