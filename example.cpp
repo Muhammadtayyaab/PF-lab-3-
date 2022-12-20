e77fc0523c44bfc9b00e662166b90de4c69aa8dc
unknown <tayyabashraf629@gmail.com>
2022-12-20 19:23:24 +0500

    msg

---
+++ example.cpp
@@ -0,0 +1,23 @@
+    
+
+#include<iostream>
+
+using namespace std ;
+
+main(){
+cout <<"My name is M Tayyab."<<endl;
+
+cout <<"My Roll no. is 115592." <<endl;
+
+cout<<"My section is C."<<endl;
+
+cout <<"Welcome to Programming Fundamentals class.";
+
+cout << endl;
+
+cout << "This is my first program.";
+
+
+
+
+}
